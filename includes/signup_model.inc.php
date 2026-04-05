abc82822457f45d3d540d06188f63f3015a4d89a
Bunveng Khang <bunvengk16@gmail.com>
2026-04-05 17:27:29 +0700

    Added new functions to singup model

---
+++ includes/signup_model.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+// no same username
+// unregistered email
+// valid email
+// ID more than 8 characters
+
+declare(strict_types=1);
+
+// gets the username from the database
+// used to check if the username is taken or not
+function get_username(object $pdo, string $username) {
+    $query = "SELECT username FROM users WHERE username = :username;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":username", $username);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+};
+
+function get_email(object $pdo, string $email) {
+    $query = "SELECT email FROM users WHERE email = :email;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+};
+
+function set_user() {
+
+};
