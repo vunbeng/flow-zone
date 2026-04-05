e4ff39f8da1ae20e1b1ac7a7129c8b111ed4306b
Bunveng Khang <bunvengk16@gmail.com>
2026-04-05 18:18:44 +0700

    Added the signup controller/Fusing into signup feature

---
+++ includes/signup.inc.php
@@ -0,0 +1,67 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    $option = [
+        'cost' => 12
+    ];
+ 
+    try {
+        require_once 'dbh.inc.php';
+        require_once 'signup_model.inc.php';
+        require_once 'signup_controller.inc.php';
+
+        // validation rules:
+            // validation rules:
+            // all input must be filled before submitting
+            // valid email
+            // no same username
+            // registered email cannot be used
+            // password more than 8 characters
+
+        $errors = []; // tracking errors; should be associative
+
+        // empty_input
+        if (is_input_empty($username, $pwd, $email)) {
+            $errors['empty_input'] = 'Fill all inputs!';
+        }
+
+        // invalid_email
+        if (is_email_invalid($email)) {
+            $errors['invalid_email'] = 'Email is invalid!';
+        }
+
+        // registered_email
+        if (is_email_registered($pdo, $email)) {
+            $errors['registered_email'] = 'Email is already registered. Use a different one!';
+        }
+
+        // error handling using validation rules
+        if ($errors) {
+            // stores the errors to be called later
+            $_SESSION['errors_signup'] = $errors;
+
+            // stores the filled inputs for later if it's valid
+            // better UX for users
+            $signupData = [
+                'username' => $username,
+                'email' => $email
+            ];
+            $_SESSION['signup_data'] = $signupData;
+
+            header('Location: ../register.php'); // goes back to the signup page
+            die();
+        }
+
+
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else {
+    header("Location: ../register.php");
+}
---
+++ includes/signup_controller.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+// validation rules:
+    // all input must be filled before submitting
+    // valid email
+    // no same username
+    // unregistered email
+    // password more than 8 characters
+
+declare(strict_types= 1);
+
+function is_input_empty(string $username, string $pwd, string $email) {
+    if (empty($username) || empty($pwd) || empty($email)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_email_invalid(string $email) {
+    // returns true if the filter cannot validate the email
+    // meaning it's not valid, marking the error
+    return filter_var($email, FILTER_VALIDATE_EMAIL) === false;
+}
+
+function is_username_taken(object $pdo, string $username) {
+    if (get_username($pdo, $username)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_email_registered(object $pdo, string $email) {
+    if (get_email($pdo, $email)) {
+        return true;
+    } else {
+        return false;
+    }
+}
